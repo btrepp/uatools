32db13d5e5bcd76ecb2626679978742eaa6a8365
Beau Trepp <beau.trepp@riotinto.com>
2016-03-15 14:05:51 +0800

    Hacky browse implemenetd

--- src/uabrowse.c
+++ src/uabrowse.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "open62541.h"
 
@@ -18,7 +19,11 @@ int main(int argc, char** argv) {
 	bReq.requestedMaxReferencesPerNode = 0;
 	bReq.nodesToBrowse = UA_BrowseDescription_new();
 	bReq.nodesToBrowseSize = 1;
+
 	bReq.nodesToBrowse[0].nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER); //browse objects folder
+    if(argc>3){
+	    bReq.nodesToBrowse[0].nodeId = UA_NODEID_STRING(atoi(argv[2]),argv[3]);
+    }
 	bReq.nodesToBrowse[0].resultMask = UA_BROWSERESULTMASK_ALL; //return everything
 
 
